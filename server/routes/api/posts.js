e2496f8aa0ef0061afac41ab489b199b672df5fd
Hatem <hatem@bentoandco.com>
2019-10-08 10:55:11 +0900

    create, retrieve, delete posts

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,16 +3,30 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const posts = await loadPostsCollection();
-  res.send(posts.find({}).toArray());
+router.get('/', async (req, res) => {
+  const postsDb = await loadPostsCollection();
+  const posts = await postsDb.find().toArray();
+  res.send(posts);
+});
+
+router.post('/', async (req, res) => {
+  const postsDb = await loadPostsCollection();
+  await postsDb.insertOne({ text: req.body.text, created_at: new Date() });
+  res.status(201).send();
+});
+
+router.delete('/:id', async (req, res) => {
+  const postsDb = await loadPostsCollection();
+  await postsDb.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
 });
 
 async function loadPostsCollection() {
   const client = await mongodb.MongoClient.connect(
-    'localhost:5001',
-    { useNewUrlParser: true }
+    'mongodb://localhost:5001/vuegram',
+    { useNewUrlParser: true, useUnifiedTopology: true }
   );
   return client.db('vuegram').collection('posts');
 }
+
 module.exports = router;
