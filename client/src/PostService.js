a2e26cb0b88e0beff9414fe3bb360e6773f25140
Hatem <hatem@bentoandco.com>
2019-10-09 13:43:09 +0900

    Client: Post service to create, get and delete posts

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -1,22 +1,28 @@
-import axios from axios;
+import axios from 'axios';
 
-const url = 'http://localhost:5000/api/posts';
+const url = 'http://localhost:5000/api/posts/';
 
-class PostService {
-  // Get Posts
-  static getPosts(){
+export default class PostService {
+  static getPosts() {
     new Promise(async (resolve, reject) => {
       try {
-        const {data} = await axios.get(url);
-        resolve (
-          data.map(post => ({
-            ...post,
-            created_at: new Date(post.created_at)
-          }))
-        )
+        const { data } = await axios.get(url);
+        const formattedPosts = data.map(post => ({
+          ...post,
+          created_at: new Date(post.created_at)
+        }));
+        resolve(formattedPosts);
       } catch (err) {
-        reject(err) 
+        reject(err);
       }
-    })
+    });
+  }
+
+  static insertPost(text) {
+    return axios.post(url, { text });
+  }
+
+  static deletePost(id) {
+    return axios.delete(`${url}${id}`);
   }
 }
